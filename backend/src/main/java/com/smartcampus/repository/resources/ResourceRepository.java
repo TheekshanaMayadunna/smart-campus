4ba5ae21fa16c836fe9e8aedc0c83c7a0aca98a4
chamodi kumarage <chamodikumarage2003@gmail.com>
2026-04-19 17:36:22 +0530

    create resource and resource image repositories

---
+++ backend/src/main/java/com/smartcampus/repository/resources/ResourceRepository.java
@@ -0,0 +1,18 @@
+package com.smartcampus.repository.resources;
+
+import com.smartcampus.model.resources.Resource;
+import com.smartcampus.model.resources.ResourceStatus;
+import com.smartcampus.model.resources.ResourceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface ResourceRepository extends JpaRepository<Resource, Long>, JpaSpecificationExecutor<Resource> {
+
+    List<Resource> findByDeletedFalse();
+
+    List<Resource> findByDeletedFalseAndType(ResourceType type);
+
+    List<Resource> findByDeletedFalseAndStatus(ResourceStatus status);
+}
