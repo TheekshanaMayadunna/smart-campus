4b11b2f388853cf5acae76a6dc094b4c911d2342
chamodi kumarage <chamodikumarage2003@gmail.com>
2026-04-19 17:36:00 +0530

    add common api response and error response dto classes

---
+++ backend/src/main/java/com/smartcampus/dto/resources/ApiResponseDto.java
@@ -0,0 +1,12 @@
+package com.smartcampus.dto.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponseDto {
+    private String message;
+}
