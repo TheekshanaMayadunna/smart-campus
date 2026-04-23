4f42fb00d8ab74449ae00e51543f5bae566ac3ea
sanujaandrahennadi <sanujaandrahennadi@gmail.com>
2026-04-24 00:46:48 +0530

    Add authentication controller endpoints

---
+++ backend/src/main/java/com/smartcampus/dto/Auth/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.smartcampus.dto.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String studentId;
+    private String role;
+    private String pictureUrl;
+    private boolean googleLogin;
+}
