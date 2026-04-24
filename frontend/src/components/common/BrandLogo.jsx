c73be71d0580543966004fa3c2d28b67816a5dd0
sanujaandrahennadi <sanujaandrahennadi@gmail.com>
2026-04-25 02:00:21 +0530

    Add brand assets and dashboard icons

---
+++ frontend/src/components/common/BrandLogo.jsx
@@ -0,0 +1,16 @@
+import defaultLogo from "../../Assests/campus-velora-logo.svg";
+
+export default function BrandLogo({
+  className = "",
+  src = defaultLogo,
+  alt = "Campus Velora logo",
+}) {
+  return (
+    <div className={`brandLogo ${className}`.trim()}>
+      <div className="brandLogoMark" aria-hidden="true">
+        <img src={src} alt={alt} className="brandLogoImg" />
+      </div>
+    </div>
+  );
+}
+
